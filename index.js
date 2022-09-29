7ead7a1f20e838e318a6c3bbcae4725a6faec01f
Владислав Никулин <63848849+valdemarnikulin@users.noreply.github.com>
2022-09-29 09:59:13 +0600

    create bot

---
+++ index.js
@@ -0,0 +1,42 @@
+const qrcode = require("qrcode-terminal");
+const cron = require("node-cron");
+
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+let date;
+
+client.on("message", async (msg) => {
+  const chat = await msg.getChat();
+  if (chat.id.user === "some telephone number") {
+    let arrMsg = msg.body.split(" ");
+    if (arrMsg.includes("newDate") && arrMsg.length == 6) {
+      date = arrMsg.slice(1).join(" ");
+    }
+    arrMsg.forEach((el) => {
+      let yes = el.replace(/^8|\+7/, "7");
+      if (!isNaN(Number(el)) && el.length == 11) {
+        cron.schedule(
+          date,
+          () => {
+            client.sendMessage(`${yes}@c.us`, "Hello");
+          },
+          {
+            scheduled: true,
+          }
+        );
+      }
+    });
+  }
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.initialize();
